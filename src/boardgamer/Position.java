207009f7ae700f7faee2f1828bd46b82051219e8
Rafael Antonelli <rafaelantonelli5@hotmail.com>
2021-04-13 14:45:14 -0300

    First Class: Position

---
+++ src/boardgamer/Position.java
@@ -0,0 +1,33 @@
+package boardgamer;
+
+public class Position {
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRoll() {
+		return row;
+	}
+
+	public void setRoll(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
